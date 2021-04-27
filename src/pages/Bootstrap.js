b9044a9fa7462f16e8f7be896c58bafdb4262132
ethanjd23 <ethanjd23@gmail.com>
2021-04-26 23:17:17 -0500

    begin

---
+++ src/pages/Bootstrap.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Bootstrap = () => {
+    return (
+        <div className="bg-success p-5">
+            <h1 className="display-1 text-center">Hi, I'm Ethan Delcambre</h1>
+            <h3 className="display-5 text-center text-muted">Full-Stack Web Developer</h3>
+        </div>
+    )
+}
+
+export default Bootstrap
