f2edd0bfdec5cd28775181e9d5337741cc1e8d57
ethanjd23 <ethanjd23@gmail.com>
2021-04-26 23:54:28 -0500

    education section begun

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Bootstrap from './pages/Bootstrap'
--- src/pages/Bootstrap.js
+++ src/pages/Bootstrap.js
@@ -1,12 +1,76 @@
-import React from 'react'
+import React from "react";
+import BootstrapAccordion from "../components/BootstrapAccordion";
 
 const Bootstrap = () => {
-    return (
-        <div className="bg-success p-5">
-            <h1 className="display-1 text-center">Hi, I'm Ethan Delcambre</h1>
-            <h3 className="display-5 text-center text-muted">Full-Stack Web Developer</h3>
+  return (
+    <>
+      <div className="bg-success p-5">
+        <h1 className="display-1 text-center">Hi, I'm Ethan Delcambre</h1>
+        <h3 className="display-5 text-center text-muted">
+          Full-Stack Web Developer
+        </h3>
+      </div>
+    <container className="container-fluid justify-content-center">
+      <div className="bg-light mx-5">
+        <h3>Education</h3>
+      </div>
+      <div className="accordion mx-5" id="accordionExample">
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="headingOne">
+            <button
+              className="accordion-button"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseOne"
+              aria-expanded="true"
+              aria-controls="collapseOne"
+            >
+              University of Alabama at Birmingham
+            </button>
+          </h2>
+          <div
+            id="collapseOne"
+            className="accordion-collapse collapse show"
+            aria-labelledby="headingOne"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body">
+              <strong>B.S. in Digital Marketing</strong><br/>
+              Concentration in Digital Marketing<br/>
+              Minor in History 
+            </div>
+          </div>
         </div>
-    )
-}
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="headingTwo">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseTwo"
+              aria-expanded="false"
+              aria-controls="collapseTwo"
+            >
+              Innovate Birmingham
+            </button>
+          </h2>
+          <div
+            id="collapseTwo"
+            className="accordion-collapse collapse"
+            aria-labelledby="headingTwo"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body">
+              <strong>Full-Stack Web Development</strong><br/>
+              Created full-stack website using MySQL, React, TypeScript, Material UI, and Node<br/>
+              Used Agile methodologies to develop in a team environment
+            </div>
+          </div>
+        </div>
+      </div>      
+      </container>
+      </>    
+  );
+};
 
-export default Bootstrap
+export default Bootstrap;
